e3e81c1b7830fd95d87429d6df76bbec8f2c3a26
Boris <borisvereb@gmail.com>
2019-10-20 16:11:13 +0200

    All done

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,18 @@
-interface Person {
-    firstName: string;
-    lastName: string;
+document.getElementById("InsertRow").addEventListener("click", AdditionToList);
+let CaseSelect : HTMLSelectElement = <HTMLSelectElement>document.getElementById("CaseSelect");
+let list = document.getElementById("list");
+let elementInput : HTMLInputElement = <HTMLInputElement>document.getElementById('myInput');
+function AdditionToList()
+{
+    let result = elementInput.value;
+    let newLi = document.createElement("li");
+    switch (CaseSelect.value) {
+        case "UpperCase":
+            newLi.innerHTML = result.toUpperCase();
+            break;
+        case "LowerCase":
+            newLi.innerHTML = result.toLowerCase();
+            break;
+    }
+    list.appendChild(newLi);
 }
-
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
-let user: Person = { firstName: "John", lastName: "Doe" };
-
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
